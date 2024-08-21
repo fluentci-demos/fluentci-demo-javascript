985751a07faf7a9165602b2af985112c8f4473aa
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-21 22:13:03 +0000

    ci: update ci.yml
    ci: update ci.yml
    
    ci: update ci.yml
    
    ci: update ci.yml
    
    ci: update ci.yml

--- .fluentci/src/lib.rs
+++ .fluentci/src/lib.rs
@@ -11,3 +11,14 @@ pub fn e2e() -> FnResult<String> {
         .stdout()?;
     Ok(stdout)
 }
+
+#[plugin_fn]
+pub fn server_e2e() -> FnResult<String> {
+    let stdout = dag()
+        .mise()?
+        .with_exec(vec![
+            "mise install && cd src/server && cp ormconfig.ci.json ormconfig.json && mise x -- bun install && mise x -- bun run migrate:up && mise x -- bun run test:e2e",
+        ])?
+        .stdout()?;
+    Ok(stdout)
+}
